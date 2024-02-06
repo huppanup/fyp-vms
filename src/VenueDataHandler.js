1f44bf68db77b17d6618f16a3b84691115b0b2e1
huppanup <94790989+huppanup@users.noreply.github.com>
2024-02-07 00:57:01 +0800

    Refractoring

--- src/VenueDataHandler.js
+++ src/VenueDataHandler.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
 import { storage } from './firebase';
 import {ref,getDownloadURL} from "firebase/storage";
 
+// Retrieves and callbacks file URL from storage path
+function getFileURL(path, callback){
+    const pathRef = ref(storage, path);
+    getDownloadURL(ref(storage, pathRef))
+        .then((url) => callback(url))
+        .catch((e) => console.log(e));
+}
+
+// Requests download from storage path and callbacks the result. Type can be either text or json.
 function downloadData(path, type, callback){
     // Data type should be either json or text, if neither it is set to text.
     if (type !== "json" && type !=="text"){
         type = "text";
     }
     // Retrieves raw text
-    const pathRef = ref(storage, path);
-    getDownloadURL(ref(storage, pathRef))
-    .then((url) => {
+    getFileURL(path, (url) => {
         const xhr = new XMLHttpRequest();
         xhr.responseType = type;
         xhr.onreadystatechange = () => {
@@ -21,12 +26,12 @@ function downloadData(path, type, callback){
           };
         xhr.open('GET', url);
         xhr.send();
-    }).catch((error) => {
-    console.log("Error while downloading. "+ error);
     });
 }
 
-// Any reformatting of the texts should be handled in these wrapper functions, inside the callback.
+// Retrieves venue info as JSON.
+// STRUCTURE
+// { venueName : string, venueID : venueID, floors : [string]}
 function getVenueInfo(venueID, callback){
     downloadData(venueID + "/info.json", "json", (data) => {
         const venueInfo = {
@@ -36,6 +41,24 @@ function getVenueInfo(venueID, callback){
         }
         callback(venueInfo);
     });
+}
+
+// Retrieves floor info as JSON.
+// STRUCTURE
+// { venueID : string, floorNo : string, floorplan : string, settings : JSON{ scale : float, deviation : int, transformation : [float]}}
+function getFloorInfo(venueID, floorNo, callback){
+    getFileURL(venueID + "/map/" + floorNo + "/map.jpg", (url) => {
+        downloadData(venueID + "/info.json", "json", (data) => {
+            const floorinfo = {
+                "venueID" : venueID,
+                "floorNo" : floorNo,
+                "floorplan" : url,
+                "settings" : data["settings"][floorNo]
+            }
+            callback(floorinfo);
+        });
+    });
+    
 }
 
 function getConstraint(venueID, floorNo, callback){ 
@@ -72,48 +95,12 @@ function getConstraint(venueID, floorNo, callback){
     });
     }
 
-export default function VenueData(id) {
+export default function VenueData(id, f) {
     this.venueID = id;
+    this.floor = f;
     this.getVenueInfo = (callback) => getVenueInfo(this.venueID, (data) => {callback(data)});
-    this.getConstraint = (floorNo, callback) => getConstraint(this.venueID,floorNo,callback);
-}
-
-
-function getAlignment(locationID, floorNo){
-// Returned JSON structure should be:
-/*
-{ value:
-    { 
-        locationID : string,
-        floorNo : int,
-        latitude : string,
-        longitude : string,
-        angle : string,
-        scale : string
-    },
-    success : boolean
-}
-*/
-    const result = {"success":true}
-    return result
-}
-
-function getFloorplan(locationID, floorNo){
-// Returned JSON structure should be:
-/*
-{ value:
-    { 
-        locationID : string,
-        floorNo : int,
-        floorName : string,
-        altitude : float?,
-        filepath : string (floorplan image file)
-    },
-    success : boolean
-}
-*/
-    const result = {"success":true}
-    return result
+    this.getFloorInfo = (callback) => getFloorInfo(this.venueID, this.floor, (data) => {callback(data)});
+    this.getConstraint = (floorNo, callback) => getConstraint(this.venueID,this.floor,callback);
 }
 
 function editConstraint(locationID, floorNo, type, id, x, y){
--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -4,13 +4,23 @@ import "../stylesheets/cloud.css";
 import VenueData from '../VenueDataHandler';
 
 export default () => {
-  const currentVenue = new VenueData('HKUST_fusion');
+  const [curVenue, setCurVenue] = useState('HKUST_fusion');
+  const [curFloor, setCurFloor] = useState('LSK1');
+
+  const venueHandler = new VenueData(curVenue, curFloor);
+  function changeFloor(venueHandler, floor){
+    console.log("Changing floors")
+    setCurFloor(floor)
+  }
   const [venueInfo, setVenueInfo] = useState();
   useEffect(() => {
-    currentVenue.getVenueInfo((data) => {
-      setVenueInfo(data);
+    venueHandler.getVenueInfo((data) => {
+      console.log(data);
     });
-  },[]);
+    venueHandler.getFloorInfo((data) => {
+      console.log(data);
+    });
+  },[curFloor]);
   
   
   
@@ -19,6 +29,7 @@ export default () => {
     <div className="main-container">
         <div className="cloud-main-panel">
         <div><a>{JSON.stringify(venueInfo)}</a></div>
+        <button onClick={() => changeFloor(venueHandler, "LSK3")}>HI CLICK ME TO CHANGE THE FLOOR</button>
         <iframe width="100%" height="100%" src="https://www.openstreetmap.org/export/embed.html?bbox=127.08180248737337%2C37.319532987776675%2C127.08387047052386%2C37.321023990244015&amp;layer=mapnik" style={ {border : "1px solid black"}}></iframe>
         </div>
   </div>
