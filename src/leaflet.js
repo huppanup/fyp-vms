efa0a2ba98a9059f5a093c9ceaf082bfd44f6bcc
Woo Jung Byun <wjbyun@connect.ust.hk>
2024-03-06 22:51:34 +0800

    Add rotation to leaflet overlay

--- src/leaflet.js
+++ src/leaflet.js
@@ -1,6 +1,7 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { transpose, multiply, inv, atan2 } from 'mathjs';
+import 'leaflet-imageoverlay-rotated';
 
 export function initializeMap() {
     const mapContainer = document.getElementById("mapContainer");
@@ -29,43 +30,57 @@ export function addFloorPlanImage(map, url, geoPoints, pixelPoints, height, widt
     if (!map) return;
     let imageUrl = url;
     let bounds = calculateBounds(geoPoints, pixelPoints, height, width);
-    let rotation = Math.atan2((bounds[1][0] - bounds[0][0]), (bounds[1][1] - bounds[0][1])) * (180 / Math.PI);
-    console.log(rotation);
-    let latLngBounds = L.latLngBounds([bounds]);
+    let bottomLeft = L.latLng(bounds[0][0], bounds[0][1]);
+	let upperRight = L.latLng(bounds[1][0], bounds[1][1]);
+	let upperLeft = L.latLng(bounds[2][0], bounds[2][1]);
+
     map.eachLayer(function(layer) {
         if (layer instanceof L.ImageOverlay) {
             map.removeLayer(layer);
         }
     });
-    /*geoPoints.forEach(element => {
+    geoPoints.forEach(element => {
         var marker = new L.marker([element[1], element[0]]).addTo(map);
-    });*/
-
-    let marker1 = L.marker(bounds[0], {
-        draggable: true
-    }).addTo(map);
-    
-    let marker2 = L.marker(bounds[1], {
-        draggable: true
-    }).addTo(map);
-    
-    let imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
+    });
+    let imageOverlay = L.imageOverlay.rotated(imageUrl, upperLeft, upperRight, bottomLeft, {
         opacity: 0.6,
         interactive: true
     }).addTo(map);
     //imageOverlay.getElement().style.transform += " rotate(" + rotation + "deg)";
+
+    let marker1 = L.marker(upperLeft, {draggable: true} ).addTo(map);
+	let marker2 = L.marker(upperRight, {draggable: true} ).addTo(map);
+	let marker3 = L.marker(bottomLeft, {draggable: true} ).addTo(map);
+    //marker1.bindPopup(marker1.getLatLng()).openPopup();
+    marker1.on('drag dragend', function () {
+        repositionImage(imageOverlay, marker1, marker2, marker3)
+    });
+    marker2.on('drag dragend', function () {
+        repositionImage(imageOverlay, marker1, marker2, marker3)
+    });
+    marker3.on('drag dragend', function () {
+        repositionImage(imageOverlay, marker1, marker2, marker3)
+    });
 }
+
+function repositionImage(imageOverlay, marker1, marker2, marker3) {
+    imageOverlay.reposition(marker1.getLatLng(), marker2.getLatLng(), marker3.getLatLng());
+};
 
 function calculateBounds(geoPoints, pixelPoints, height, width) {
     const matrix = calculateAffineTransformationMatrix(pixelPoints, geoPoints);
 
+    console.log(matrix);
+
     const lowerLeft = applyAffineTransform(matrix, 0, height);
     const upperRight = applyAffineTransform(matrix, width, 0);
+    const upperLeft = applyAffineTransform(matrix, 0, 0);
 
     console.log(lowerLeft);
     console.log(upperRight);
+    console.log(upperLeft);
 
-    return [[lowerLeft.latitude, lowerLeft.longitude], [upperRight.latitude, upperRight.longitude]];
+    return [[lowerLeft.latitude, lowerLeft.longitude], [upperRight.latitude, upperRight.longitude], [upperLeft.latitude, upperLeft.longitude]];
 }
 
 function calculateAffineTransformationMatrix(pixelPoints, geoPoints) {
@@ -101,6 +116,7 @@ function calculateAffineTransformationMatrix(pixelPoints, geoPoints) {
 
 function applyAffineTransform(matrix, x, y) {
     const result = multiply(matrix, [x, y, 1]);
+    console.log(result);
   
     const longitude = result[0];
     const latitude = result[1];
